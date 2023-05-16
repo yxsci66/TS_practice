41568c2ecd7e3178e1ccc47743dcbde4842385e7
yxsci66 <yxsci66@163.com>
2023-05-16 21:07:02 +0800

    feat add challenges

---
+++ 00270-hard-typed-get.ts
@@ -0,0 +1,28 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Get<Data, "hello">, "world">>,
+  Expect<Equal<Get<Data, "foo.bar.count">, 6>>,
+  Expect<Equal<Get<Data, "foo.bar">, { value: "foobar"; count: 6 }>>,
+  Expect<Equal<Get<Data, "foo.baz">, false>>,
+
+  Expect<Equal<Get<Data, "no.existed">, never>>
+];
+
+type Data = {
+  foo: {
+    bar: {
+      value: "foobar";
+      count: 6;
+    };
+    included: true;
+  };
+  "foo.baz": false;
+  hello: "world";
+};
+
+// ============= Your Code Here =============
+type Get<T, K extends string> = K extends `${infer F}.${infer R}`
+  ? Get<T[F & keyof T], R>
+  : T[K & keyof T];
