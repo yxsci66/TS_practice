64579e0e656fe3be5e57038448c4c6e3b9ac237a
yxsci66 <yxsci66@163.com>
2023-06-01 14:06:45 +0800

    feat add challenges

--- 00270-hard-typed-get.ts
+++ 00270-hard-typed-get.ts
@@ -23,6 +23,8 @@ type Data = {
 };
 
 // ============= Your Code Here =============
-type Get<T, K extends string> = K extends `${infer F}.${infer R}`
+type Get<T, K extends string> = K extends keyof T
+  ? T[K]
+  : K extends `${infer F}.${infer R}`
   ? Get<T[F & keyof T], R>
   : T[K & keyof T];
